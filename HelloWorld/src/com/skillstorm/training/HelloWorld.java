54e72e84b3eef67b5687aaa4f2d7f247fe112615
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-11-13 14:24:53 -0500

    day1 code demo

---
+++ HelloWorld/src/com/skillstorm/training/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.skillstorm.training;
+
+// This is a comment that the compiler ignores it's just for humans reading this file
+// Shortcuts:
+//  F11 runs the program (or use the green arrow button)
+//  CTRL-FORWARD SLASH  comments out the line
+//  sysout CTRL-SPACE will generate System.out.println();
+
+/*
+ * This is a multiline comment 
+ * It's great for longer descriptions
+ */
+
+/*
+ * Important syntax:
+ *   - wrap all your code in curly braces
+ *   - 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println();
+	}
+
+}
