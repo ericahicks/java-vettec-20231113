6053f28c3d1d726da60c27d8e356a8b221ac5c81
cahumada3 <110481231+cahumada3@users.noreply.github.com>
2023-12-20 13:29:51 -0500

    week 6 day 3, Collections API

---
+++ week06/Day3/Collections/CollectionsAPI.java
@@ -0,0 +1,23 @@
+package Day3.Collections;
+
+/*
+ * Collections API
+ *      -> refers to a set of class and interfaces that provide a way
+ *         to work with a collection of objects
+ *      -> offers a set of data structure to and algorithms to store,
+ *         manage, and manipulate groups of objects
+ * 
+ *      -> Collections framework includes:
+ *              - Interfaces: List, Set, Queue, etc.
+ *              - Classes: ArrayList, LinkedList, HashSet, HashMap, PriorityQueue, etc.
+ * 
+ *      -> Overall, Colections API provides a standardized way of dealing with a collection
+ *         of objects efficiently 
+ * 
+ * 
+ */
+
+
+public class CollectionsAPI {
+    
+}
